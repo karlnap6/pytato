5818b9823852c48e19c464dbc93fbdafcf25ac51
Chang <mailcz8@gmail.com>
2022-03-29 19:29:35 -0700

    add files

--- cDad/demo.py
+++
@@ -1,7 +0,0 @@
-def test_list(lis, show):
-    for i in range(show):
-        print(lis[i])
-
-test_list(['red', 'blue', 'yellow', 'green', 'purple', 'black', 'gold', 'orange'], 3)
-
-name = input("What is your name? ")
